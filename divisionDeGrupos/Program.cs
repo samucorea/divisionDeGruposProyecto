7be9b83216f9dbb10286f0198269b12fb95537a0
samucorea <samuel.alejandro100@hotmail.com>
2021-05-16 16:55:38 -0400

    Init

---
+++ divisionDeGrupos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace divisonDeGrupos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
